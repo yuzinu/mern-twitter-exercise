ee13fb28e8d2ab05c1a65b334bf945abdce5259f
Eugene Moon <moonyoojin@gmail.com>
2020-10-04 19:41:19 -0400

    Complete project without CSS

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const app = express();
+const db = require("./config/keys").mongoURI;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-mongodb+srv://admin:<password>@mern.dl0sq.mongodb.net/<dbname>?retryWrites=true&w=majority
+const User = require("./models/User");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(err => console.log(err));
+
+// app.get("/", (req, res) => {
+//   const user = new User({
+//     handle: "juniper",
+//     email: "juniper@juniper.com",
+//     password: "juniper"
+//   });
+//   user.save();
+//   res.send("Hello World!");
+// });
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+  
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
