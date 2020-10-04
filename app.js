e9b0cdafc485fcce305cda358f84b6bbb8382c31
Eugene Moon <moonyoojin@gmail.com>
2020-10-03 21:18:09 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+mongodb+srv://admin:<password>@mern.dl0sq.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
